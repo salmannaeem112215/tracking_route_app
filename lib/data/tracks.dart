cdf0172fe60762197731527b1394ee352f6d6d42
salmannaeem112215 <salman.naeem.112215@gmail.com>
2023-02-11 13:56:04 +0500

    Added Comments

--- lib/data/tracks.dart
+++ lib/data/tracks.dart
@@ -5,15 +5,10 @@ import 'package:tracking_route_app/models/time.dart';
 import '../models/track.dart';
 
 class Tracks extends ChangeNotifier {
-  Tracks.database(List<Track> t) {
-    _tracks = t;
-  }
-  Tracks();
-
   List<Track> _tracks = [
     // Default Track 1
     Track(
-      name: 'Route 5',
+      name: '5',
       isMorning: true,
       stops: [
         Stop(
@@ -45,7 +40,7 @@ class Tracks extends ChangeNotifier {
     ),
     // Default Track 2
     Track(
-      name: 'Route 5',
+      name: '5',
       isMorning: false,
       stops: [
         Stop(
@@ -77,35 +72,74 @@ class Tracks extends ChangeNotifier {
     )
   ];
 
+  // Constructor
+  Tracks();
+  Tracks.database(List<Track> t) {
+    _tracks = t;
+  }
+
+  //Getter
+  // get Track List
   List<Track> getTracks() {
     return _tracks;
   }
 
+  // get Track With Name
+  Track getTrack(String name, bool isMorning) {
+    final track =
+        _tracks.firstWhere((element) => element.isMatched(name, isMorning));
+
+    return track;
+  }
+  ////////////////////////////
+  // CRUD OPERATIONS
+  ///////////////////////////
+
+  // Add Track in Track List
   void addTrack({required String trackName, required bool isMorning}) {
-    int counter = trackExist(trackName, isMorning);
+    // getting counter value
+    int counter = getCounter(trackName, isMorning);
+
+    // 0 means new Name so no need to change name
     if (counter != 0) {
-      trackName += '_$counter'; // route 5_1
+      trackName = _counterFormat(trackName, counter); // route 5_1
     }
+
+    // Add Track in Track List
     _tracks
         .add(Track(name: trackName, isMorning: isMorning, stops: [], path: []));
     notifyListeners();
   }
 
-  // return the digit that tell how many time it and it's copy exist
-  int trackExist(String name, bool isMorning, {int count = 0}) {
-    String _name = name;
-    int count = 0;
-    for (int i = 0; i < _tracks.length; i++) {
-      if (_tracks[i].isMatched(_name, isMorning)) {
-        count++;
-        _name = '${name}_$count';
-      }
-    }
-    return count;
-  }
-
+  // Remove Track from TrackList
   void removeTrack(int index) {
     _tracks.removeAt(index);
     notifyListeners();
+  }
+
+  //////////////////////
+  // Utility Functions
+  //////////////////////
+
+  // This function takes route name and counter to return a format
+  String _counterFormat(String name, int count) {
+    return '$name ($count)';
+  }
+
+  // return the counter value for new Track to be add
+  int getCounter(String name, bool isMorning, {int count = 0}) {
+    String _name = name;
+    int count = 0;
+
+    // Iterate for each Item in Tracks
+    for (int i = 0; i < _tracks.length; i++) {
+      if (_tracks[i].isMatched(_name, isMorning)) {
+        count++;
+        _name = _counterFormat(name, count);
+      }
+    }
+
+    // Return counter value
+    return count;
   }
 }
