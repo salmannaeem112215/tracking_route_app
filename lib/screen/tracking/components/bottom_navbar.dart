57f02047ec1bc600de9119e097186ac466fbcabd
salmannaeem112215 <salman.naeem.112215@gmail.com>
2023-02-11 19:32:15 +0500

    Animation Stated

--- lib/screen/tracking/components/bottom_navbar.dart
+++ lib/screen/tracking/components/bottom_navbar.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rive/rive.dart';
 
-class BottomNavbar extends StatelessWidget {
+class BottomNavbar extends StatefulWidget {
   const BottomNavbar({super.key});
+
+  @override
+  State<BottomNavbar> createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> {
+  late RiveAnimationController _buttonController;
+
+  @override
+  void initState() {
+    _buttonController = SimpleAnimation("Demo");
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +42,24 @@ class BottomNavbar extends StatelessWidget {
               child: const Text('Add Stop'),
             ),
           )),
-          IconButton(
-              onPressed: () {},
-              color: Colors.white,
-              icon: Icon(
-                // Icons.play_circle,
-                Icons.pause_circle_outline_rounded,
-              )),
+          GestureDetector(
+            onTap: () {
+              print('button press');
+              setState(() {
+                _buttonController = SimpleAnimation("Demo 2");
+              });
+            },
+            child: SizedBox(
+              height: 70,
+              width: 250,
+              child: RiveAnimation.asset(
+                'assets/rive/play_pause_button.riv',
+                controllers: [
+                  _buttonController,
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
